6bceb7090ff36cb55f283a1f2d7365243e303947
andreamartinez21 <andreamartinez21@opendeusto.es>
2021-11-21 15:53:54 +0100

    Session creados getters etc

--- Server/src/es/deusto/ingenieria/sd/auctions/server/data/domain/Session.java
+++ Server/src/es/deusto/ingenieria/sd/auctions/server/data/domain/Session.java
@@ -8,5 +8,89 @@ public class Session {
 	private String iDate;
 	private String iHour;
 	private String duration;
+
+	public Session(int id, String tittle, String sport, double distance, String iDate, String iHour, String duration) {
+		super();
+		this.id = id;
+		this.tittle = tittle;
+		this.sport = sport;
+		this.distance = distance;
+		this.iDate = iDate;
+		this.iHour = iHour;
+		this.duration = duration;
+	}
+
+	public Session() {
+		super();
+		this.id = 0;
+		this.tittle = "";
+		this.sport = "";
+		this.distance = 0.00;
+		this.iDate = "";
+		this.iHour = "";
+		this.duration = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTittle() {
+		return tittle;
+	}
+
+	public void setTittle(String tittle) {
+		this.tittle = tittle;
+	}
+
+	public String getSport() {
+		return sport;
+	}
+
+	public void setSport(String sport) {
+		this.sport = sport;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public String getiDate() {
+		return iDate;
+	}
+
+	public void setiDate(String iDate) {
+		this.iDate = iDate;
+	}
+
+	public String getiHour() {
+		return iHour;
+	}
+
+	public void setiHour(String iHour) {
+		this.iHour = iHour;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	
+	@Override
+	public String toString() {
+		return "Session [id=" + id + ", tittle=" + tittle + ", sport=" + sport + ", distance=" + distance + ", iDate="
+				+ iDate + ", iHour=" + iHour + ", duration=" + duration + "]";
+	}
 	
 }
