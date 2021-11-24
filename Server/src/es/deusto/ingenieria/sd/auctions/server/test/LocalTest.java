47f2506a25e4630a176e8756c70c27081f236ce7
ssc1099 <ssc1099.csmm@gmail.com>
2021-11-24 20:32:48 +0100

    LocalTest created

--- Server/src/es/deusto/ingenieria/sd/auctions/server/test/LocalTest.java
+++ Server/src/es/deusto/ingenieria/sd/auctions/server/test/LocalTest.java
@@ -2,48 +2,31 @@ package es.deusto.ingenieria.sd.auctions.server.test;
 
 import java.util.List;
 
-import es.deusto.ingenieria.sd.auctions.server.data.dto.ArticleDTO;
-import es.deusto.ingenieria.sd.auctions.server.data.dto.CategoryDTO;
+import es.deusto.ingenieria.sd.auctions.server.data.domain.LocalUser;
+import es.deusto.ingenieria.sd.auctions.server.data.dto.SessionDTO;
+import es.deusto.ingenieria.sd.auctions.server.data.dto.UserDTO;
 import es.deusto.ingenieria.sd.auctions.server.remote.RemoteFacade;
 
 public class LocalTest {
 
 	public static void main(String[] args) {		
 		RemoteFacade facade = null;
-		List<> categories = null;
-		CategoryDTO category = null;
-		List<ArticleDTO> articles = null;
-		ArticleDTO article = null;
+		SessionDTO session = null;
+		List<SessionDTO> sessions= null;
+		UserDTO user = null;
+		LocalUser localUser = null;
 		long token = 0l;
 		
 		try {
 			facade = new RemoteFacade();
 			
-			//Get Categories
-			categories = facade.getCategories();
-			category = categories.get(0);
+			//Get Sessions of a User
+			sessions = facade.getListSessions(token);
+			sessions = (List<SessionDTO>) sessions.get(0);
 			
-			for (CategoryDTO c : categories) {
-				System.out.println("\t- " + c);
-			}			
-						
-			//Get Articles of a Category
-			articles = facade.getArticles(category.getName());
-			article = articles.get(0);
-			
-			for (ArticleDTO a : articles) {
-				System.out.println("\t- " + a);
-			}
-			
-			//Check currency conversion
-			float rateGBP = facade.getGBPRate();
-			float rateUSD = facade.getUSDRate();
-			
-			System.out.println("\t- USD rate = " + rateGBP);
-			System.out.println("\t- USD rate = " + rateUSD);
-			
-			//Make a bid (fails because no login has been done)
-			facade.makeBid(0, article.getNumber(), article.getActualPrice()+1);						
+			for (SessionDTO s : sessions) {
+				System.out.println("\t- " + s);
+			}								
 		} catch (Exception e) {			
 			System.out.println("\t# Error: " + e.getMessage());
 		} 
@@ -51,15 +34,18 @@ public class LocalTest {
 		try {
 			//Login
 			String sha1 = org.apache.commons.codec.digest.DigestUtils.sha1Hex("$!9PhNz,");
-			token = facade.login("thomas.e2001@gmail.com", sha1);			
-			//Make a bid
-			facade.makeBid(token, article.getNumber(), article.getActualPrice()+1);
+			token = facade.login("google", "thomas.e2001@gmail.com", sha1);			
+			//Register
+			facade.register(localUser);
 			//Logout
 			facade.logout(token);
-			//Get articles again to check if the bid has been registered
-			articles = facade.getArticles(category.getName());
-			article = articles.get(0); 			
-			System.out.println("\t- " + article);
+			//Get sessions to check if they have been created
+			session = facade.getSession(token, session.getId());
+			sessions = facade.getListSessions(token);
+			System.out.println("\t- " + session);
+			//Get user
+			user = facade.getUser(token);
+			
 		} catch (Exception e) {
 			System.out.println("\t# Error: " + e.getMessage());	
 		}
